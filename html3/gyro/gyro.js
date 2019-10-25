4739f2223d3f896add6fd877a37a89493f87d1c8
AlexandraDemeter <56438504+AlexandraDemeter@users.noreply.github.com>
2019-10-25 13:40:06 +0300

    acceleration added

--- html3/gyro/gyro.js
+++ html3/gyro/gyro.js
@@ -5,4 +5,4 @@ function on_rotate_uab(e)
 	document.getElementById("id_alpha").innerHTML = e.alpha;
 	document.getElementById("id_beta").innerHTML = e.beta;
 	document.getElementById("id_gamma").innerHTML = e.gamma;
-}
+} 
